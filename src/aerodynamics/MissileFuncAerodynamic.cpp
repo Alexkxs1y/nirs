acf5f2da9b4c128c687de4bcf7dd0448b9f38e96
alipatov <lipatov.aleksey.lav@gmail.com>
2024-12-03 06:49:49 +0300

    fix sign in stabilization equation and fix mistakes in FuncAerodynamic

--- src/aerodynamics/MissileFuncAerodynamic.cpp
+++ src/aerodynamics/MissileFuncAerodynamic.cpp
@@ -7,7 +7,7 @@
 using namespace std; 
 
 MissileFuncAerodynamic::MissileFuncAerodynamic(){
-    rad2deg = 180.0/M_PI;
+    rad2deg = 1;// 180.0/M_PI;
 }
 
 MissileFuncAerodynamic::~MissileFuncAerodynamic(){}
@@ -16,7 +16,7 @@ double MissileFuncAerodynamic::get_cx(double M, std::vector<double> &alpha_beta)
     if (M < 2.16) {
 		M = 2.16;
 	}
-	return 0.5 * (1 / (73.211 / exp(M) - 47.483 / M + 16.878));
+	return (1 / (73.211 / exp(M) - 47.483 / M + 16.878));
 }
 
 double MissileFuncAerodynamic::get_cy_a(double M) const{
@@ -35,7 +35,7 @@ double MissileFuncAerodynamic::get_cy_delta(double M, double alpha) const{
 	double p2 = log(1.9773 * alpha_deg * alpha_deg - 25.587 * alpha_deg + 83.354);
 	double p3 = 18.985 * alpha_deg * alpha_deg - 375.76 * alpha_deg + 1471;
 	double p4 = -51.164e-03 * alpha_deg * alpha_deg + 805.52e-03 * alpha_deg + 1.8929;
-	return (-p1 * 1e-06 * M * M + p2 * 1e-12 * exp(M) + p3 * 1e-06 * M - p4 * 1e-03) * 2;
+	return (-p1 * 1e-06 * M * M + p2 * 1e-12 * exp(M) - p3 * 1e-06 * M - p4 * 1e-03) * 2;////////////////
 }
 
 double MissileFuncAerodynamic::get_cz_b(double M) const{
@@ -74,28 +74,28 @@ double MissileFuncAerodynamic::get_my_delta(double M, double beta) const{
 }
 
 double MissileFuncAerodynamic::get_cy(double M, std::vector<double> &alpha_beta, std::vector<double> &deltas) const{
-    return 0.1* (get_cy_a(M) * alpha_beta[0] * rad2deg + get_cy_delta(M, alpha_beta[0])* deltas[2] * rad2deg);
+    return (get_cy_a(M) * alpha_beta[0] * rad2deg + get_cy_delta(M, alpha_beta[0])* deltas[2] * rad2deg);
 }
 
 double MissileFuncAerodynamic::get_cz(double M, std::vector<double> &alpha_beta, std::vector<double> &deltas) const{
-    return 0.1*(get_cz_b(M) * alpha_beta[1] * rad2deg + get_cz_delta(M, alpha_beta[1]) * deltas[1] * rad2deg);
+    return (get_cz_b(M) * alpha_beta[1] * rad2deg + get_cz_delta(M, alpha_beta[1]) * deltas[1] * rad2deg);
 }
 
 double MissileFuncAerodynamic::get_mx(double M, std::vector<double> &alpha_beta, std::vector<double> &deltas, std::vector<double> &w, double l_div_Vabs) const{
     if(w[0] == 0){
         return 0;
     }
-    return -0.005 * 0.6786 * l_div_Vabs; 
+    return -0.005 * 0.6786 * l_div_Vabs * w[0]; 
 }
 
 double MissileFuncAerodynamic::get_my(double M, std::vector<double> &alpha_beta, std::vector<double> &deltas, std::vector<double> &w, double l_div_Vabs) const{
-    return 0.1*(get_my_wy(M) * w[1] * l_div_Vabs + get_my_b(M) * alpha_beta[1] * rad2deg + get_my_delta(M, alpha_beta[1]) * deltas[1] * rad2deg); 
+    return (get_my_wy(M) * w[1] * l_div_Vabs + get_my_b(M) * alpha_beta[1] * rad2deg + get_my_delta(M, alpha_beta[1]) * deltas[1] * rad2deg); 
 }
 
 double MissileFuncAerodynamic::get_mz(double M, std::vector<double> &alpha_beta, std::vector<double> &deltas, std::vector<double> &w, double l_div_Vabs) const{
-    return 0.1*(get_mz_a(M) * alpha_beta[0] * rad2deg + get_mz_wz(M) * w[2] * l_div_Vabs + get_mz_delta(M, alpha_beta[0]) * deltas[2] * rad2deg);
+    return (get_mz_a(M) * alpha_beta[0] * rad2deg + get_mz_wz(M) * w[2] * l_div_Vabs + get_mz_delta(M, alpha_beta[0]) * deltas[2] * rad2deg);
 }
 
 double MissileFuncAerodynamic::get_mStab() const{
-    return 80;
+    return 100;
 }
